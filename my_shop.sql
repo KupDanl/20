d29ac92384329f617175b8e7508330bebcf194d0
KupDanl <mrgandghubas@gmail.com>
2018-05-28 20:44:56 +0300

    upd last

--- my_shop.sql
+++ my_shop.sql
@@ -1,1 +1,410 @@
-SELECT * FROM my_shop.products;
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Май 28 2018 г., 20:41
+-- Версия сервера: 5.6.38
+-- Версия PHP: 5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `my_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'summer'),
+(2, 'winter'),
+(3, 'all-season');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `currency`
+--
+
+CREATE TABLE `currency` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `currency`
+--
+
+INSERT INTO `currency` (`id`, `name`) VALUES
+(1, 'dollars'),
+(2, 'euros'),
+(3, 'BT'),
+(4, 'punds');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `customtype_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `customtype_id`) VALUES
+(1, 'John', 1),
+(2, '\"F3\"', 2),
+(3, 'Emily', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `customtype`
+--
+
+CREATE TABLE `customtype` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `discount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `customtype`
+--
+
+INSERT INTO `customtype` (`id`, `type`, `discount`) VALUES
+(1, 'retail', NULL),
+(2, 'wholesale', 15);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `manager`
+--
+
+CREATE TABLE `manager` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `pass` int(11) DEFAULT NULL,
+  `post_id` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `manager`
+--
+
+INSERT INTO `manager` (`id`, `name`, `pass`, `post_id`) VALUES
+(1, 'Andrew', 1, 2),
+(2, 'Adam', 2, 1),
+(3, 'Bill', 3, 1),
+(4, 'Stew', 4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `products_id` int(11) UNSIGNED NOT NULL,
+  `manager_id` int(11) UNSIGNED NOT NULL,
+  `customers_id` int(11) UNSIGNED NOT NULL,
+  `paytype_id` int(11) UNSIGNED NOT NULL,
+  `currency_id` int(11) UNSIGNED NOT NULL,
+  `amount` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `products_id`, `manager_id`, `customers_id`, `paytype_id`, `currency_id`, `amount`) VALUES
+(1, 2, 3, 1, 1, 2, 2),
+(2, 1, 1, 2, 2, 3, 400),
+(3, 2, 1, 2, 2, 3, 200),
+(4, 4, 2, 1, 1, 2, 4),
+(5, 3, 4, 3, 1, 1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `payment_type`
+--
+
+CREATE TABLE `payment_type` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `payment_type`
+--
+
+INSERT INTO `payment_type` (`id`, `name`) VALUES
+(1, 'cash'),
+(2, 'card'),
+(3, 'C.O.D.');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `post` varchar(255) NOT NULL,
+  `salary` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `post`
+--
+
+INSERT INTO `post` (`id`, `post`, `salary`) VALUES
+(1, 'manager', 8000),
+(2, 'general manager', 16000),
+(3, 'assistant', 3000);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `producer`
+--
+
+CREATE TABLE `producer` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `producer`
+--
+
+INSERT INTO `producer` (`id`, `name`) VALUES
+(1, 'Premiorri '),
+(2, 'Bridgestone '),
+(3, 'GoodYear'),
+(4, 'Michelin ');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `producer_id` int(11) UNSIGNED NOT NULL,
+  `category_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `model`, `producer_id`, `category_id`) VALUES
+(1, 'ViaMaggiore ', '185/60 R14 82T', 1, 2),
+(2, 'B-250 ', '165/65 R14 79T', 2, 1),
+(3, 'Eagle Sport ', '185/60 R14 82H', 3, 1),
+(4, 'X-Ice Xi3 ', '155/65 R13 73T', 4, 2);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `currency`
+--
+ALTER TABLE `currency`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_customtype_id_idx` (`customtype_id`);
+
+--
+-- Индексы таблицы `customtype`
+--
+ALTER TABLE `customtype`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_post_id_idx` (`post_id`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_products_id_idx` (`products_id`),
+  ADD KEY `fk_manager_id_idx` (`manager_id`),
+  ADD KEY `fk_customers_id_idx` (`customers_id`),
+  ADD KEY `fk_paytype_id_idx` (`paytype_id`),
+  ADD KEY `fk_currency_id_idx` (`currency_id`);
+
+--
+-- Индексы таблицы `payment_type`
+--
+ALTER TABLE `payment_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `producer`
+--
+ALTER TABLE `producer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_producer_id_idx` (`producer_id`),
+  ADD KEY `fk_category_id_idx` (`category_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `currency`
+--
+ALTER TABLE `currency`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `customtype`
+--
+ALTER TABLE `customtype`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `manager`
+--
+ALTER TABLE `manager`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `payment_type`
+--
+ALTER TABLE `payment_type`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `producer`
+--
+ALTER TABLE `producer`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `fk_customtype_id` FOREIGN KEY (`customtype_id`) REFERENCES `customtype` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `manager`
+--
+ALTER TABLE `manager`
+  ADD CONSTRAINT `fk_post_id` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_currency_id` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_customers_id` FOREIGN KEY (`customers_id`) REFERENCES `customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_manager_id` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_paytype_id` FOREIGN KEY (`paytype_id`) REFERENCES `payment_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_products_id` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_producer_id` FOREIGN KEY (`producer_id`) REFERENCES `producer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
